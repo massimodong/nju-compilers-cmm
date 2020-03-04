18eedc76580c0f6f166c47eca542d43ee4f0b0ff
Massimo Dong <dongmassimo@gmail.com>
2020-03-04 18:27:39 +0800

    print syntax tree

--- Code/tree.c
+++ Code/tree.c
@@ -2,6 +2,9 @@
 #include <assert.h>
 #include <stdlib.h>
 #include "common.h"
+#include "syntax.tab.h"
+
+extern char *IDs[];
 
 void treeInit(Tree **tp, int st){
   Tree *t = malloc(sizeof(Tree));
@@ -24,32 +27,75 @@ void treeInit(Tree **tp, int st){
   */
 }
 
-void printNode(int x, int s){
-  for(int i=0;i<x;++i) printf("  ");
-  if(s > 250) printf("<Token>\n"); //TODO: magic number here
-  else printf("%s\n", syntaxName[s]);
+void printToken(int s, Treep t){
+  switch(s){
+    case INT: printf("INT: %d\n", t->int_val); break;
+    case FLOAT: printf("FLOAT: %f\n", t->float_val); break;
+    case ID: printf("ID: %s\n", IDs[t->int_val]); break;
+    case SEMI: printf("SEMI\n"); break;
+    case COMMA: printf("COMMA\n"); break;
+    case TYPE: printf("TYPE: %s\n", t->int_val ? "float" : "int"); break;
+    case LC: printf("LC\n"); break;
+    case RC: printf("RC\n"); break;
+    case STRUCT: printf("STRUCT\n"); break;
+    case RETURN: printf("RETURN\n"); break;
+    case IF: printf("IF\n"); break;
+    case ELSE: printf("ELSE\n"); break;
+    case WHILE: printf("WHILE\n"); break;
+    case ASSIGNOP: printf("ASSIGNOP\n"); break;
+    case OR: printf("OR\n"); break;
+    case AND: printf("AND\n"); break;
+    case RELOP: printf("RELOP\n"); break;
+    case PLUS: printf("PLUS\n"); break;
+    case MINUS: printf("MINUS\n"); break;
+    case STAR: printf("STAR\n"); break;
+    case DIV: printf("DIV\n"); break;
+    case NOT: printf("NOT\n"); break;
+    case LP: printf("LP\n"); break;
+    case RP: printf("RP\n"); break;
+    case LB: printf("LB\n"); break;
+    case RB: printf("RB\n"); break;
+    case DOT: printf("DOT\n"); break;
+    default: assert(0);
+  }
 }
 
-void treeCheckEmpty(Tree *t){
-  if(t->stype > 250){ //TODO: magic number here
+void printNode(int x, int s, Tree *t){
+  for(int i=0;i<x;++i) printf("  ");
+  if(s > 250){
+    printToken(s, t);
+  }else{
+    printf("%s (%d)\n", syntaxName[s], t->lineno);
+  }
+}
+
+void treePreDfs(Tree *t){
+  if(t->stype >= INT){
     t->show = 1;
     return;
   }
 
+  int foundch = 0;
   for(int i=0;i<MAXCH;++i) if(t->ch[i]){
-    treeCheckEmpty(t->ch[i]);
+    treePreDfs(t->ch[i]);
     if(t->ch[i]->show) t->show = 1;
+
+    if(foundch){
+    }else{
+      foundch = 1;
+      t->lineno = t->ch[i]->lineno;
+    }
   }
 }
 
 void treeDfs(Tree *t, int s, int x){
   //printf("dfsing %d, %d\n", (int)t, s);
   if(!t->show) return;
-  printNode(x, s);
+  printNode(x, s, t);
   for(int i=0;i<MAXCH;++i) if(t->ch[i]) treeDfs(t->ch[i], t->ch[i]->stype, x+1);
 }
 
 void treePrint(Tree *t){
-  treeCheckEmpty(t);
+  treePreDfs(t);
   treeDfs(t, Program, 0);
 }
