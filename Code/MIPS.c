028803a567fbd8c1a6d24630bef24b51a32789d0
Massimo Dong <dongmassimo@gmail.com>
2020-05-29 11:28:07 +0800

    print prompt

--- Code/MIPS.c
+++ Code/MIPS.c
@@ -9,7 +9,7 @@ extern int label_cnt;
 
 static const char *START_TEXT[] = {
   ".data",
-  "_prompt: .asciiz \"Please input an integer:\\n\"",
+  "_prompt: .asciiz \"Enter an integer:\"",
   "_ret: .asciiz \"\\n\"",
   ".globl main",
   ".text",
@@ -164,9 +164,9 @@ static void translateStmt(IRCode irc){
       break;
 
     case OP_READ:
-      //fprintf(fir, "  la $a0, _prompt\n");
-      //fprintf(fir, "  li $v0, 4\n");
-      //fprintf(fir, "  syscall\n");
+      fprintf(fir, "  la $a0, _prompt\n");
+      fprintf(fir, "  li $v0, 4\n");
+      fprintf(fir, "  syscall\n");
 
       fprintf(fir, "  li $v0, 5\n");
       fprintf(fir, "  syscall\n");
